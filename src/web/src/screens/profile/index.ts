a48bf935d7071661fcc52fb56bed25e75c17cfe8
Alexander Blessing <alex.c.blessing@gmail.com>
2025-03-11 05:00:47 -0600

    Add public-samples/mydoc-jfobwo/src/web/src/screens/profile/index.ts

---
+++ src/web/src/screens/profile/index.ts
@@ -0,0 +1,15 @@
+/**
+ * Index file for exporting the ProfileScreen component
+ * Provides a centralized entry point for importing the profile screen
+ * 
+ * The ProfileScreen displays user information and provides logout functionality,
+ * implementing the F-005 User Profile Management requirement. It shows the user's
+ * email, account creation date, and app version information.
+ * 
+ * @module ProfileScreen
+ */
+
+import ProfileScreen from './ProfileScreen';
+
+export { ProfileScreen };
+export default ProfileScreen;
