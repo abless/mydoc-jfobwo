31048a5079155b6dd9d871f112c3028224b93731
Alexander Blessing <alex.c.blessing@gmail.com>
2025-03-11 04:59:35 -0600

    Add public-samples/mydoc-jfobwo/src/web/src/components/modals/index.ts

---
+++ src/web/src/components/modals/index.ts
@@ -0,0 +1,13 @@
+/**
+ * index.ts
+ * 
+ * Central export file for all modal components used in the Health Advisor application.
+ * This file aggregates all modal-related components to provide a convenient import point,
+ * allowing consumers to import any modal component from a single location.
+ */
+
+import Modal from './Modal';
+import BottomSheet from './BottomSheet';
+import ConfirmationModal from './ConfirmationModal';
+
+export { Modal, BottomSheet, ConfirmationModal };
