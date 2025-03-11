7091461b134cdfb6384290a693e7fac016d3cc45
Alexander Blessing <alex.c.blessing@gmail.com>
2025-03-11 04:59:24 -0600

    Add public-samples/mydoc-jfobwo/src/web/src/components/forms/index.ts

---
+++ src/web/src/components/forms/index.ts
@@ -0,0 +1,15 @@
+/**
+ * Form components barrel file
+ * 
+ * This file exports all form-related components from the forms directory,
+ * providing a centralized import point for form components used throughout
+ * the Health Advisor mobile application.
+ */
+
+// Import form components
+import FormField from './FormField';
+import RadioGroup from './RadioGroup';
+import TextInput from './TextInput';
+
+// Re-export form components for use throughout the application
+export { FormField, RadioGroup, TextInput };
