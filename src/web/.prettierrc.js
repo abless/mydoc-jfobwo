fe8182cbcaf22dcf18ab2cb067a5368c018de947
Alexander Blessing <alex.c.blessing@gmail.com>
2025-03-11 04:56:41 -0600

    Add public-samples/mydoc-jfobwo/src/web/.prettierrc.js

---
+++ src/web/.prettierrc.js
@@ -0,0 +1,28 @@
+module.exports = {
+  // Specifies the line length that the printer will wrap on
+  printWidth: 100,
+
+  // Specifies the number of spaces per indentation level
+  tabWidth: 2,
+
+  // Use single quotes instead of double quotes
+  singleQuote: true,
+
+  // Print trailing commas wherever possible in multi-line comma-separated structures
+  trailingComma: 'es5',
+
+  // Add semicolons at the end of statements
+  semi: true,
+
+  // Print spaces between brackets in object literals
+  bracketSpacing: true,
+
+  // Put the > of a multi-line JSX element at the end of the last line
+  jsxBracketSameLine: false,
+
+  // Include parentheses around a sole arrow function parameter
+  arrowParens: 'always',
+
+  // Specify the end of line character
+  endOfLine: 'lf',
+};
