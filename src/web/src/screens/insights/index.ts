0e13041a48754582f0753119fe6f6dda9c1a8b23
Alexander Blessing <alex.c.blessing@gmail.com>
2025-03-11 05:00:44 -0600

    Add public-samples/mydoc-jfobwo/src/web/src/screens/insights/index.ts

---
+++ src/web/src/screens/insights/index.ts
@@ -0,0 +1,12 @@
+/**
+ * Index file for the Insights screen
+ * 
+ * This file exports the InsightsScreen component for use in the Health Advisor
+ * mobile application's navigation system. It serves as a centralized entry point
+ * for importing the Insights screen component.
+ */
+
+import InsightsScreen from './InsightsScreen';
+
+export { InsightsScreen };
+export default InsightsScreen;
